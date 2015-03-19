b06f09e4fc87ac78884cf8eed435adccfd860834
Andre Kampert <andre@product-foundry.com>
2015-03-19 11:06:06 +0100

    Await recovery of memory image

--- core/src/main/scala/com/productfoundry/akka/cqrs/MemoryImage.scala
+++ core/src/main/scala/com/productfoundry/akka/cqrs/MemoryImage.scala
@@ -1,13 +1,18 @@
 package com.productfoundry.akka.cqrs
 
 import akka.actor.{ActorLogging, ActorRefFactory, Props}
-import akka.persistence.{RecoveryFailure, PersistentView, Update}
+import akka.persistence._
 
 import scala.concurrent.stm.{Ref, _}
 
 object MemoryImage {
   def apply[State, Event <: AggregateEvent](actorRefFactory: ActorRefFactory, persistenceId: String)(initialState: State)(update: (State, Commit[Event]) => State) = {
     new MemoryImage(actorRefFactory, persistenceId)(initialState)(update)
+  }
+
+  object RecoveryStatus extends Enumeration {
+    type RecoveryStatus = Value
+    val Unstarted, Started, Failed, Completed = Value
   }
 }
 
@@ -16,6 +21,9 @@ object MemoryImage {
  * committed events onto the current state.
  */
 class MemoryImage[State, -Event <: AggregateEvent] private (actorRefFactory: ActorRefFactory, persistenceId: String)(initialState: State)(update: (State, Commit[Event]) => State) {
+  import MemoryImage.RecoveryStatus
+  
+  private val recoveryStatus: Ref[RecoveryStatus.RecoveryStatus] = Ref(RecoveryStatus.Unstarted)
   private val state: Ref[State] = Ref(initialState)
   private val revision: Ref[DomainRevision] = Ref(DomainRevision.Initial)
   private val ref = actorRefFactory.actorOf(Props(new MemoryImageActor(persistenceId)))
@@ -45,6 +53,17 @@ class MemoryImage[State, -Event <: AggregateEvent] private (actorRefFactory: Act
   }
 
   /**
+   * Blocks until initial recovery of the memory image is complete.
+   */
+  def awaitRecover(): Unit = {
+    atomic { implicit txn =>
+      if (recoveryStatus() != RecoveryStatus.Completed) {
+        retry
+      }
+    }
+  }
+
+  /**
    * Applies the given commit to the memory image.
    * @param domainCommit to apply.
    */
@@ -58,9 +77,22 @@ class MemoryImage[State, -Event <: AggregateEvent] private (actorRefFactory: Act
   class MemoryImageActor(val persistenceId: String) extends PersistentView with ActorLogging {
     override val viewId: String = s"$persistenceId-view"
 
+    override def preStart(): Unit = {
+      super.preStart()
+      recoveryStatus.single.set(RecoveryStatus.Started)
+    }
+
     override def receive: Receive = {
-      case commit: DomainCommit[Event] => update(commit)
-      case RecoveryFailure(cause) => log.error(cause, "Unable to recover: {}", persistenceId)
+      case commit: DomainCommit[Event] =>
+        update(commit)
+
+      case RecoveryCompleted =>
+        log.info("Memory image recovery completed: {}", persistenceId)
+        recoveryStatus.single.set(RecoveryStatus.Completed)
+
+      case RecoveryFailure(cause) =>
+        log.error(cause, "Unable to recover: {}", persistenceId)
+        recoveryStatus.single.set(RecoveryStatus.Failed)
     }
   }
 }
