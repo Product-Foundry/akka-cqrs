f20b84e24388ebe1c394065ff462eae520c1548f
Andre Kampert <andre@product-foundry.com>
2015-03-21 14:44:34 +0100

    Not limited to memory projections

--- core/src/main/scala/com/productfoundry/akka/cqrs/Projection.scala
+++ core/src/main/scala/com/productfoundry/akka/cqrs/Projection.scala
@@ -1,15 +1,15 @@
 package com.productfoundry.akka.cqrs
 
 /**
- * Build memory projections.
+ * Build projections.
  */
-trait MemoryProjection[P] {
+trait Projection[P] {
 
   type Project = PartialFunction[AggregateEvent, P]
 
   def project(revision: AggregateRevision): Project
 
-  def projectOnto[C <: MemoryProjection[C]](revision: AggregateRevision, event: AggregateEvent, projection: C): Option[C] = {
+  def projectOnto[C <: Projection[C]](revision: AggregateRevision, event: AggregateEvent, projection: C): Option[C] = {
     val projectFunction = projection.project(revision)
     if (projectFunction.isDefinedAt(event)) Some(projectFunction(event)) else None
   }
