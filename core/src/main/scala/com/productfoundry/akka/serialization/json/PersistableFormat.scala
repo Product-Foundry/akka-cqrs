3f336fa19b97c04038cf81ceb648525b8c5571d2
Andre Kampert <andre@product-foundry.com>
2015-05-28 13:24:11 +0200

    Simplify persistable format

--- core/src/main/scala/com/productfoundry/akka/serialization/json/PersistableFormat.scala
+++ core/src/main/scala/com/productfoundry/akka/serialization/json/PersistableFormat.scala
@@ -1,15 +1,20 @@
 package com.productfoundry.akka.serialization.json
 
 import com.productfoundry.akka.cqrs._
+import com.productfoundry.akka.cqrs.project.DomainCommit
 import com.productfoundry.akka.cqrs.publish.ConfirmationProtocol
-import play.api.libs.json.Format
+import play.api.libs.json.{Format, Json}
 
 trait PersistableFormat {
 
+  implicit def CommitFormat(implicit eventFormat: Format[AggregateEvent]): Format[Commit] = Json.format[Commit]
+
+  implicit def DomainCommitFormat(implicit eventFormat: Format[AggregateEvent]): Format[DomainCommit] = Json.format[DomainCommit]
+
   implicit def AggregateEventFormatToPersistableFormat(implicit eventFormat: Format[AggregateEvent]): Format[Persistable] = {
     TypeChoiceFormat(
-      "Commit" -> CommitFormat.CommitFormat,
-      "DomainCommit" -> DomainCommitFormat.DomainCommitFormat,
+      "Commit" -> CommitFormat,
+      "DomainCommit" -> DomainCommitFormat,
       "Confirmed" -> ConfirmationProtocol.Confirmed.ConfirmedFormat
     )
   }
