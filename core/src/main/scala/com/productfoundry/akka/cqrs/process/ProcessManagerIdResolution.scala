56839fe551f3acf9802f538a1cba2f9cab05276e
Andre Kampert <andre@product-foundry.com>
2015-06-12 16:09:31 +0200

    Document purpose

--- core/src/main/scala/com/productfoundry/akka/cqrs/process/ProcessManagerIdResolution.scala
+++ core/src/main/scala/com/productfoundry/akka/cqrs/process/ProcessManagerIdResolution.scala
@@ -2,4 +2,8 @@ package com.productfoundry.akka.cqrs.process
 
 import com.productfoundry.akka.cqrs.EntityIdResolution
 
+/**
+ * Process manager ids are different for every process and define which process instances receive
+ * a message. Multiple processes can receive the same messages with possible a different process manager id.
+ */
 trait ProcessManagerIdResolution[A <: ProcessManager[_, _]] extends EntityIdResolution[A]
