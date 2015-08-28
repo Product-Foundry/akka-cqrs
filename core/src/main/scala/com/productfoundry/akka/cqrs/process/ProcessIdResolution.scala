8c75eea9cd0db664de0cba4503ddd1d2a62eaba6
Andre Kampert <andre@product-foundry.com>
2015-08-28 17:36:47 +0200

    Add process ID resolution

---
+++ core/src/main/scala/com/productfoundry/akka/cqrs/process/ProcessIdResolution.scala
@@ -0,0 +1,29 @@
+package com.productfoundry.akka.cqrs.process
+
+import com.productfoundry.akka.cqrs.{AggregateEventRecord, AggregateEvent, EntityIdResolution}
+import com.productfoundry.akka.cqrs.EntityIdResolution._
+import com.productfoundry.akka.cqrs.publish.EventPublication
+
+/**
+ * Process id resolution only makes sense for the actual event data.
+ *
+ * Since there is some complexity around publication, this trait provides a convenience entity id resolver that
+ * simplifies this process.
+ */
+trait ProcessIdResolution[A] extends EntityIdResolution[A] {
+
+  type ProcessIdResolver = PartialFunction[AggregateEvent, String]
+
+  def processIdResolver: ProcessIdResolver
+
+  override def entityIdResolver: EntityIdResolver = {
+    case eventPublication: EventPublication if processIdResolver.isDefinedAt(eventPublication.eventRecord.event) =>
+      processIdResolver(eventPublication.eventRecord.event)
+
+    case eventRecord: AggregateEventRecord if processIdResolver.isDefinedAt(eventRecord.event) =>
+      processIdResolver(eventRecord.event)
+
+    case event: AggregateEvent if processIdResolver.isDefinedAt(event) =>
+      processIdResolver(event)
+  }
+}
