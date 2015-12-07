e983fa812b34690b369be84005ada6abcc9acab0
Andre Kampert <andre@product-foundry.com>
2015-12-07 12:41:29 +0100

    Cluster singleton entity context

---
+++ cluster/src/main/scala/com/productfoundry/akka/cluster/ClusterSingletonEntityContext.scala
@@ -0,0 +1,43 @@
+package com.productfoundry.akka.cluster
+
+import akka.actor._
+import akka.cluster.singleton.{ClusterSingletonManager, ClusterSingletonManagerSettings, ClusterSingletonProxy, ClusterSingletonProxySettings}
+import akka.util.Timeout
+import com.productfoundry.akka.cqrs.EntityContextActor.GetOrCreateSupervisor
+import com.productfoundry.akka.cqrs._
+
+import scala.concurrent.Await
+import scala.language.implicitConversions
+import scala.reflect.ClassTag
+
+/**
+  * Entity context that uses a cluster singleton to handle all updates on a single node.
+  */
+class ClusterSingletonEntityContext(system: ActorSystem, actorName: String = "Domain") extends EntityContext {
+
+  val singleton = system.actorOf(ClusterSingletonManager.props(
+    singletonProps = Props[EntityContextActor],
+    terminationMessage = PoisonPill,
+    settings = ClusterSingletonManagerSettings(system)),
+    name = actorName)
+
+  val singletonProxy = system.actorOf(ClusterSingletonProxy.props(
+    singletonManagerPath = s"/user/$actorName",
+    settings = ClusterSingletonProxySettings(system)),
+    name = s"${actorName}Proxy")
+
+  override def entitySupervisorFactory[E <: Entity : EntityFactory : EntityIdResolution : ClassTag]: EntitySupervisorFactory[E] = {
+    new EntitySupervisorFactory[E] {
+      override def getOrCreate: ActorRef = {
+        import akka.pattern.ask
+
+        import scala.concurrent.duration._
+
+        implicit val timeout = Timeout(30.seconds)
+
+        val supervisorRefFuture = (singletonProxy ? GetOrCreateSupervisor(Props(new LocalEntitySupervisor[E]), supervisorName)).mapTo[ActorRef]
+        Await.result(supervisorRefFuture, timeout.duration)
+      }
+    }
+  }
+}
