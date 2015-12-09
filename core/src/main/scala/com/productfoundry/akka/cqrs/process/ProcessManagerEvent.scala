0c56687c0b1ae5b2b2900cf7c151137a21cd0825
Andre Kampert <andre@product-foundry.com>
2015-12-09 16:48:09 +0100

    No need for process manager events to contain id

--- core/src/main/scala/com/productfoundry/akka/cqrs/process/ProcessManagerEvent.scala
+++ core/src/main/scala/com/productfoundry/akka/cqrs/process/ProcessManagerEvent.scala
@@ -1,13 +1,6 @@
 package com.productfoundry.akka.cqrs.process
 
-import com.productfoundry.akka.cqrs.{EntityId, EntityMessage}
-
 /**
   * Process manager event used for persisting process state
   */
-trait ProcessManagerEvent extends EntityMessage {
-
-  type Id <: EntityId
-
-  def id: Id
-}
+trait ProcessManagerEvent extends Serializable
