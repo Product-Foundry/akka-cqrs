1de316defe40b96e09f2a2893d194a8da35ea41b
Andre Kampert <andre@product-foundry.com>
2015-12-10 23:55:19 +0100

    Back in-memory storage with actor

--- inmem/src/main/scala/akka/persistence/inmem/journal/InMemoryJournal.scala
+++ inmem/src/main/scala/akka/persistence/inmem/journal/InMemoryJournal.scala
@@ -1,43 +1,125 @@
 package akka.persistence.inmem.journal
 
+import akka.actor.{Props, Actor}
+import akka.persistence.inmem.journal.JournalActor._
 import akka.persistence.journal.{AsyncWriteJournal, Tagged}
 import akka.persistence.{AtomicWrite, PersistentRepr}
+import akka.pattern.ask
 import akka.serialization.SerializationExtension
+import akka.util.Timeout
 
 import scala.collection.immutable.Seq
 import scala.concurrent.Future
-import scala.util.Try
+import scala.concurrent.duration._
+import scala.util.{Success, Try}
 
-object InMemoryJournal {
-  final val Identifier = "in-memory-journal"
+class JournalActor extends Actor {
+
+  var storage = JournalStorage()
+
+  override def receive: Receive = {
+
+    case GetHighestSequenceNr(persistenceId) =>
+      sender() ! HighestSequenceNrResponse(storage.getStream(persistenceId).highestSequenceNr)
+
+    case FindByPersistenceId(persistenceId, fromSequenceNr, toSequenceNr, max) =>
+      val stream = storage.getStream(persistenceId)
+      val entries = stream.entries.filter(entry => entry.sequenceNr >= fromSequenceNr && entry.sequenceNr <= toSequenceNr)
+      val result = if (max < Int.MaxValue.toLong) entries.take(max.toInt) else entries
+      sender() ! JournalEntries(result)
+
+    case AddEntries(entries) =>
+      storage = entries.foldLeft(storage)(_ addEntry _)
+      sender() ! JournalAck
+
+    case RemoveEntries(persistenceId, toSequenceNr) =>
+      storage = storage.removeEntries(persistenceId, toSequenceNr)
+      sender() ! JournalAck
+  }
+}
+
+object JournalActor {
+
+  case class JournalEntry(persistenceId: String,
+                          sequenceNr: Long,
+                          bytes: Array[Byte],
+                          tags: Set[String] = Set.empty)
+
+  case class JournalStream(persistenceId: String,
+                           highestSequenceNr: Long = 0L,
+                           entries: Vector[JournalEntry] = Vector.empty)
+
+  case class JournalStorage(streams: Map[String, JournalStream] = Map.empty) {
+
+    def addEntry(entry: JournalEntry): JournalStorage = {
+      val stream = getStream(entry.persistenceId)
+      val updated = stream.copy(
+        highestSequenceNr = Math.max(stream.highestSequenceNr, entry.sequenceNr),
+        entries = stream.entries :+ entry
+      )
+      copy(streams = streams.updated(entry.persistenceId, updated))
+    }
+
+    def removeEntries(persistenceId: String, toSequenceNr: Long): JournalStorage = {
+      val stream = getStream(persistenceId)
+      val updated = stream.copy(
+        entries = stream.entries.filter(_.sequenceNr > toSequenceNr)
+      )
+      copy(streams = streams.updated(persistenceId, updated))
+    }
+
+    def getStream(persistenceId: String): JournalStream = {
+      streams.getOrElse(persistenceId, JournalStream(persistenceId))
+    }
+  }
+
+  case class GetHighestSequenceNr(persistenceId: String)
+
+  case class HighestSequenceNrResponse(highestSequenceNr: Long)
+
+  case class FindByPersistenceId(persistenceId: String, fromSequenceNr: Long, toSequenceNr: Long, max: Long)
+
+  case class JournalEntries(entries: Iterable[JournalEntry])
+
+  case class AddEntries(entries: Iterable[JournalEntry])
+
+  case class RemoveEntries(persistenceId: String, toSequenceNr: Long)
+
+  case object JournalAck
+
 }
 
 class InMemoryJournal extends AsyncWriteJournal {
 
-  val storage = InMemoryJournalStorageExtension(context.system)
+  val journal = context.actorOf(Props(classOf[JournalActor]))
 
   val serialization = SerializationExtension(context.system)
 
   implicit val executionContext = context.dispatcher
 
+  implicit val timeout = Timeout(5.seconds)
+
+  def deserialize(entry: JournalEntry): PersistentRepr = {
+    serialization
+      .deserialize(entry.bytes, classOf[PersistentRepr])
+      .getOrElse(throw new IllegalStateException(s"Unable to deserialize entry: $entry"))
+  }
+
   override def asyncReplayMessages(persistenceId: String, fromSequenceNr: Long, toSequenceNr: Long, max: Long)(replayCallback: (PersistentRepr) => Unit): Future[Unit] = {
-    Future {
-      storage.findByPersistenceId(persistenceId, fromSequenceNr, toSequenceNr, max)
-        .map(entry => serialization.deserialize(entry.bytes, classOf[PersistentRepr]).get)
-        .foreach(replayCallback)
+    journal ? FindByPersistenceId(persistenceId, fromSequenceNr, toSequenceNr, max) map {
+      case JournalEntries(entries) =>
+        entries.map(deserialize).foreach(replayCallback)
     }
   }
 
   override def asyncReadHighestSequenceNr(persistenceId: String, fromSequenceNr: Long): Future[Long] = {
-    Future {
-      storage.highestSequenceNr(persistenceId)
+    journal ? GetHighestSequenceNr(persistenceId) map {
+      case HighestSequenceNrResponse(highestSequenceNr) => highestSequenceNr
     }
   }
 
   override def asyncWriteMessages(messages: Seq[AtomicWrite]): Future[Seq[Try[Unit]]] = {
-
     val serialized = messages.map { atomicWrite =>
-
       val entryAttempts = atomicWrite.payload.map { persistentRepr =>
         val (persistent, tags) = persistentRepr.payload match {
           case Tagged(_payload, _tags) =>
@@ -58,22 +140,23 @@ class InMemoryJournal extends AsyncWriteJournal {
       }
 
       entryAttempts.foldLeft(Try(Vector.empty[JournalEntry])) { case (acc, entryAttempt) =>
-          acc.flatMap(entries => entryAttempt.map(entry => entries :+ entry))
+        acc.flatMap(entries => entryAttempt.map(entry => entries :+ entry))
       }
     }
 
-    Future {
-      serialized.map(_.map(storage.addEntries))
+    val batchResults = serialized.collect {
+      case Success(entries) => journal ? AddEntries(entries)
     }
+
+    Future.sequence(batchResults).map(_ => serialized.map(a => a.map(_ => ())))
   }
 
   override def asyncDeleteMessagesTo(persistenceId: String, toSequenceNr: Long): Future[Unit] = {
-    Future {
-      storage.removeEntries(persistenceId, toSequenceNr)
-    }
+    journal ? RemoveEntries(persistenceId, toSequenceNr) map (_ => ())
   }
+}
 
-  override def postStop(): Unit = {
-    storage.clear()
-  }
+object InMemoryJournal {
+
+  final val Identifier = "in-memory-journal"
 }
--- inmem/src/main/scala/akka/persistence/inmem/snapshot/InMemorySnapshotStore.scala
+++ inmem/src/main/scala/akka/persistence/inmem/snapshot/InMemorySnapshotStore.scala
@@ -1,48 +1,133 @@
 package akka.persistence.inmem.snapshot
 
+import akka.actor.{Actor, Props}
+import akka.pattern.ask
+import akka.persistence.inmem.snapshot.SnapshotStoreActor._
 import akka.persistence.serialization.Snapshot
 import akka.persistence.snapshot.SnapshotStore
 import akka.persistence.{SelectedSnapshot, SnapshotMetadata, SnapshotSelectionCriteria}
 import akka.serialization.SerializationExtension
+import akka.util.Timeout
 
 import scala.concurrent.Future
+import scala.concurrent.duration._
+
+class SnapshotStoreActor extends Actor {
+
+  var storage = SnapshotStorage()
+
+  override def receive: Receive = {
+
+    case FindLatestByCriteria(persistenceId, criteria) =>
+      sender() ! FindLatestResult(storage.findLatestByCriteria(persistenceId, criteria))
+
+    case AddEntry(entry) =>
+      storage = storage.addEntry(entry)
+      sender() ! SnapshotStoreAck
+
+    case RemoveByMetadata(metadata) =>
+      storage = storage.removeByMetadata(metadata)
+      sender() ! SnapshotStoreAck
+
+    case RemoveBySelectionCriteria(persistenceId, criteria) =>
+      storage = storage.removeBySelectionCriteria(persistenceId, criteria)
+      sender ! SnapshotStoreAck
+
+  }
+}
+
+object SnapshotStoreActor {
+
+  case class SnapshotEntry(metadata: SnapshotMetadata,
+                           bytes: Array[Byte])
+
+  case class SnapshotStream(persistenceId: String,
+                            entries: Vector[SnapshotEntry] = Vector.empty)
+
+  case class SnapshotStorage(streams: Map[String, SnapshotStream] = Map.empty) {
+
+    private def getStream(persistenceId: String): SnapshotStream = {
+      streams.getOrElse(persistenceId, SnapshotStream(persistenceId))
+    }
+
+    private def updateStream(persistenceId: String)(update: (SnapshotStream) => SnapshotStream): SnapshotStorage = {
+      copy(streams = streams.updated(persistenceId, update(getStream(persistenceId))))
+    }
+
+    def addEntry(entry: SnapshotEntry): SnapshotStorage = {
+      updateStream(entry.metadata.persistenceId) { stream =>
+        stream.copy(entries = entry +: stream.entries)
+      }
+    }
+
+    def findLatestByCriteria(persistenceId: String, criteria: SnapshotSelectionCriteria): Option[SnapshotEntry] = {
+      getStream(persistenceId).entries.find { entry =>
+        entry.metadata.sequenceNr <= criteria.maxSequenceNr && entry.metadata.timestamp <= criteria.maxTimestamp
+      }
+    }
+
+    def removeByMetadata(metadata: SnapshotMetadata): SnapshotStorage = {
+      updateStream(metadata.persistenceId) { stream =>
+        stream.copy(entries = stream.entries.filterNot { entry =>
+          entry.metadata.sequenceNr == metadata.sequenceNr &&
+            (entry.metadata.timestamp == metadata.timestamp || metadata.timestamp == 0)
+        })
+      }
+    }
+
+    def removeBySelectionCriteria(persistenceId: String, criteria: SnapshotSelectionCriteria): SnapshotStorage = {
+      updateStream(persistenceId) { stream =>
+        stream.copy(entries = stream.entries.filterNot { entry =>
+          entry.metadata.sequenceNr <= criteria.maxSequenceNr && entry.metadata.timestamp <= criteria.maxTimestamp
+        })
+      }
+    }
+  }
+
+  case class FindLatestByCriteria(persistenceId: String, criteria: SnapshotSelectionCriteria)
+
+  case class FindLatestResult(entryOption: Option[SnapshotEntry])
+
+  case class AddEntry(entry: SnapshotEntry)
+
+  case class RemoveByMetadata(metadata: SnapshotMetadata)
+
+  case class RemoveBySelectionCriteria(persistenceId: String, criteria: SnapshotSelectionCriteria)
+
+  object SnapshotStoreAck
+
+}
 
 class InMemorySnapshotStore extends SnapshotStore {
 
-  val storage = InMemorySnapshotStorageExtension(context.system)
+  val snapshotStore = context.actorOf(Props(classOf[SnapshotStoreActor]))
 
   val serialization = SerializationExtension(context.system)
 
   implicit val executionContext = context.dispatcher
 
+  implicit val timeout = Timeout(5.seconds)
+
   override def loadAsync(persistenceId: String, criteria: SnapshotSelectionCriteria): Future[Option[SelectedSnapshot]] = {
-    Future {
-      storage.findLatestByCriteria(persistenceId, criteria).map { case entry =>
-        SelectedSnapshot(entry.metadata, serialization.deserialize(entry.bytes, classOf[Snapshot]).get.data)
-      }
+    snapshotStore ? FindLatestByCriteria(persistenceId, criteria) map {
+      case FindLatestResult(entryOption) =>
+        entryOption.map { entry =>
+          SelectedSnapshot(entry.metadata, serialization.deserialize(entry.bytes, classOf[Snapshot]).get.data)
+        }
     }
   }
 
   override def saveAsync(metadata: SnapshotMetadata, data: Any): Future[Unit] = {
-    Future {
-      val snapshot = Snapshot(data)
-      storage.addEntry(SnapshotEntry(metadata, serialization.findSerializerFor(snapshot).toBinary(snapshot)))
-    }
+    val snapshot = Snapshot(data)
+    val entry = SnapshotEntry(metadata, serialization.findSerializerFor(snapshot).toBinary(snapshot))
+    snapshotStore ? AddEntry(entry) map (_ => ())
   }
 
   override def deleteAsync(metadata: SnapshotMetadata): Future[Unit] = {
-    Future {
-      storage.removeByMetadata(metadata)
-    }
+    snapshotStore ? RemoveByMetadata(metadata) map (_ => ())
   }
 
   override def deleteAsync(persistenceId: String, criteria: SnapshotSelectionCriteria): Future[Unit] = {
-    Future {
-      storage.removeBySelectionCriteria(persistenceId, criteria)
-    }
-  }
-
-  override def postStop(): Unit = {
-    storage.clear()
+    snapshotStore ? RemoveBySelectionCriteria(persistenceId, criteria) map (_ => ())
   }
 }
