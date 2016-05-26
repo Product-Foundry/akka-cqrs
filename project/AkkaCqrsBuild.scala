2ccdc5f70febf06775c85fd1f1a4b96b306b1dc4
Andre Kampert <andre@product-foundry.com>
2016-05-26 17:32:28 +0200

    Release 0.3

--- project/AkkaCqrsBuild.scala
+++ project/AkkaCqrsBuild.scala
@@ -9,7 +9,7 @@ object AkkaCqrsBuild extends Build {
 
   lazy val buildSettings = Dependencies.Versions ++ Seq(
     organization := "com.productfoundry",
-    version := "0.2.2"
+    version := "0.3"
   )
 
   lazy val root = Project(
