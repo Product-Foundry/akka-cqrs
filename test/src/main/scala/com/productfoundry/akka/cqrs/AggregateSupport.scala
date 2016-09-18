14e288130ff34cfeef81aeb3a38c6d65dce7361f
Andre Kampert <andre@product-foundry.com>
2016-09-18 13:13:24 +0200

    Do not automatically track revisions for tests

--- test/src/main/scala/com/productfoundry/akka/cqrs/AggregateSupport.scala
+++ test/src/main/scala/com/productfoundry/akka/cqrs/AggregateSupport.scala
@@ -4,20 +4,19 @@ import java.util.UUID
 
 import akka.actor._
 import com.productfoundry.akka.cqrs.AggregateStatus.AggregateStatus
-import com.productfoundry.akka.cqrs.CommandRequest._
 import org.scalatest._
 
 import scala.concurrent.stm._
 import scala.reflect.ClassTag
-import scala.util.Random
 
 /**
- * Base spec for testing aggregates.
- * @param _system test actor system.
- * @param aggregateClass aggregate class.
- * @param aggregateFactory aggregate factory, typically defined in the Spec to mixin additional behavior.
- * @tparam A Aggregate type.
- */
+  * Base spec for testing aggregates.
+  *
+  * @param _system          test actor system.
+  * @param aggregateClass   aggregate class.
+  * @param aggregateFactory aggregate factory, typically defined in the Spec to mixin additional behavior.
+  * @tparam A Aggregate type.
+  */
 abstract class AggregateSupport[A <: Aggregate](_system: ActorSystem)(implicit aggregateClass: ClassTag[A],
                                                                       aggregateFactory: AggregateFactory[A])
   extends EntitySupport(_system) {
@@ -25,43 +24,43 @@ abstract class AggregateSupport[A <: Aggregate](_system: ActorSystem)(implicit a
   implicit def entityIdResolution: EntityIdResolution[A] = new AggregateIdResolution[A]()
 
   /**
-   * Test local entities by default, requires implicit entity factory.
-   */
+    * Test local entities by default, requires implicit entity factory.
+    */
   implicit val supervisorFactory = new LocalEntityContext(system).entitySupervisorFactory[A]
 
   /**
-   * Entity supervisor for the actor under test.
-   */
+    * Entity supervisor for the actor under test.
+    */
   var supervisor: ActorRef = system.deadLetters
 
   /**
-   * Commits are collected only if the LocalCommitPublisher is mixed into the actor under test.
-   */
+    * Commits are collected only if the LocalCommitPublisher is mixed into the actor under test.
+    */
   def withCommitCollector[E](block: (LocalCommitCollector) => E): E = {
     block(commitCollector)
   }
 
   /**
-   * Optionally collected commits.
-   */
+    * Optionally collected commits.
+    */
   var commitCollectorOption: Option[LocalCommitCollector] = None
 
   /**
-   * Collected commits.
-   */
+    * Collected commits.
+    */
   def commitCollector: LocalCommitCollector = commitCollectorOption.getOrElse(throw new IllegalArgumentException("Commit collector is not yet available"))
 
   /**
-   * Initialize the supervisor.
-   */
+    * Initialize the supervisor.
+    */
   before {
     supervisor = EntitySupervisor.forType[A]
     commitCollectorOption = Some(LocalCommitCollector(UUID.randomUUID().toString))
   }
 
   /**
-   * Terminates all actors.
-   */
+    * Terminates all actors.
+    */
   after {
     terminateConfirmed(supervisor)
 
@@ -72,10 +71,11 @@ abstract class AggregateSupport[A <: Aggregate](_system: ActorSystem)(implicit a
   }
 
   /**
-   * Dump commits on failure when collected.
-   * @param test to run.
-   * @return outcome.
-   */
+    * Dump commits on failure when collected.
+    *
+    * @param test to run.
+    * @return outcome.
+    */
   override protected def withFixture(test: NoArgTest): Outcome = {
     val outcome = super.withFixture(test)
 
@@ -89,10 +89,11 @@ abstract class AggregateSupport[A <: Aggregate](_system: ActorSystem)(implicit a
   }
 
   /**
-   * Asserts a specified event is committed.
-   * @param event that is expected.
-   * @param CommitTag indicates commit type with events.
-   */
+    * Asserts a specified event is committed.
+    *
+    * @param event     that is expected.
+    * @param CommitTag indicates commit type with events.
+    */
   def expectEvent(event: AggregateEvent, headersOption: Option[CommitHeaders] = None)(implicit CommitTag: ClassTag[Commit]): Unit = {
     eventually {
       withCommitCollector { commitCollector =>
@@ -106,12 +107,12 @@ abstract class AggregateSupport[A <: Aggregate](_system: ActorSystem)(implicit a
   }
 
   /**
-   * Asserts an event is committed that matches the specified partial function.
-   *
-   * For all matching events, an assertion can be executed.
-   *
-   * @param eventRecordCheckFunction to match and assert events.
-   */
+    * Asserts an event is committed that matches the specified partial function.
+    *
+    * For all matching events, an assertion can be executed.
+    *
+    * @param eventRecordCheckFunction to match and assert events.
+    */
   def expectEventRecordPF(eventRecordCheckFunction: PartialFunction[AggregateEventRecord, Unit]): Unit = {
     eventually {
       withCommitCollector { commitCollector =>
@@ -124,12 +125,12 @@ abstract class AggregateSupport[A <: Aggregate](_system: ActorSystem)(implicit a
   }
 
   /**
-   * Asserts an event is committed that matches the specified partial function.
-   *
-   * For all matching events, an assertion can be executed.
-   *
-   * @param eventCheckFunction to match and assert events.
-   */
+    * Asserts an event is committed that matches the specified partial function.
+    *
+    * For all matching events, an assertion can be executed.
+    *
+    * @param eventCheckFunction to match and assert events.
+    */
   def expectEventPF(eventCheckFunction: PartialFunction[AggregateEvent, Unit]): Unit = {
     expectEventRecordPF {
       case eventRecord if eventCheckFunction.isDefinedAt(eventRecord.event) => eventCheckFunction(eventRecord.event)
@@ -137,9 +138,10 @@ abstract class AggregateSupport[A <: Aggregate](_system: ActorSystem)(implicit a
   }
 
   /**
-   * Maps a matching event to a value.
-   * @param eventRecordMapFunction to map an event to a value.
-   */
+    * Maps a matching event to a value.
+    *
+    * @param eventRecordMapFunction to map an event to a value.
+    */
   def mapEventRecordPF[E](eventRecordMapFunction: PartialFunction[AggregateEventRecord, E]): E = {
     eventually {
       withCommitCollector { commitCollector =>
@@ -152,9 +154,10 @@ abstract class AggregateSupport[A <: Aggregate](_system: ActorSystem)(implicit a
   }
 
   /**
-   * Maps a matching event to a value.
-   * @param eventMapFunction to map an event to a value.
-   */
+    * Maps a matching event to a value.
+    *
+    * @param eventMapFunction to map an event to a value.
+    */
   def mapEventPF[E](eventMapFunction: PartialFunction[AggregateEvent, E]): E = {
     mapEventRecordPF {
       case eventRecord if eventMapFunction.isDefinedAt(eventRecord.event) => eventMapFunction(eventRecord.event)
@@ -162,36 +165,40 @@ abstract class AggregateSupport[A <: Aggregate](_system: ActorSystem)(implicit a
   }
 
   /**
-   * Asserts a success message is sent from the aggregate.
-   * @return the success message.
-   */
+    * Asserts a success message is sent from the aggregate.
+    *
+    * @return the success message.
+    */
   def expectMsgSuccess: AggregateStatus.Success = {
     expectMsgType[AggregateStatus.Success]
   }
 
   /**
-   * Asserts a failure message is sent from the aggregate.
-   * @param t wrapped error type tag.
-   * @tparam T wrapped error type.
-   * @return the error wrapped in the failure message.
-   */
+    * Asserts a failure message is sent from the aggregate.
+    *
+    * @param t wrapped error type tag.
+    * @tparam T wrapped error type.
+    * @return the error wrapped in the failure message.
+    */
   def expectMsgError[T](implicit t: ClassTag[T]): T = {
     expectMsgType[AggregateStatus.Failure].cause.asInstanceOf[T]
   }
 
   /**
-   * Asserts a validation error is sent from the aggregate.
-   * @param message the expected validation message.
-   */
+    * Asserts a validation error is sent from the aggregate.
+    *
+    * @param message the expected validation message.
+    */
   def expectMsgValidationError(message: ValidationMessage) = {
     assertValidationError(message, expectMsgType[AggregateStatus])
   }
 
   /**
-   * Asserts a status contains a failure message.
-   * @param message the expected failure message.
-   * @param status the status.
-   */
+    * Asserts a status contains a failure message.
+    *
+    * @param message the expected failure message.
+    * @param status  the status.
+    */
   def assertValidationError(message: ValidationMessage, status: AggregateStatus): Unit = {
     status match {
       case success: AggregateStatus.Success =>
@@ -209,10 +216,11 @@ abstract class AggregateSupport[A <: Aggregate](_system: ActorSystem)(implicit a
   }
 
   /**
-   * Asserts a status contains a failure.
-   * @tparam C the expected failure class.
-   * @param status the status.
-   */
+    * Asserts a status contains a failure.
+    *
+    * @tparam C the expected failure class.
+    * @param status the status.
+    */
   def assertFailure[C: ClassTag](status: AggregateStatus): Unit = {
     status match {
       case success: AggregateStatus.Success => fail(s"Unexpected success: $success")
@@ -222,50 +230,41 @@ abstract class AggregateSupport[A <: Aggregate](_system: ActorSystem)(implicit a
   }
 
   /**
-   * Scoped fixture to setup aggregates and send messages while keeping track of revisions.
-   */
+    * Scoped fixture to setup aggregates and send messages.
+    */
   trait AggregateFixture {
-    val revisionRef = Ref(AggregateRevision.Initial)
-
     /**
-     * Use commands to initialize fixture state, asserts that all commands return success.
-     *
-     * Can be invoked multiple times.
-     *
-     * @param commands to send to aggregate, must succeed,
-     */
+      * Use commands to initialize fixture state, asserts that all commands return success.
+      *
+      * Can be invoked multiple times.
+      *
+      * @param commands to send to aggregate, must succeed,
+      */
     def given(commands: AggregateCommandMessage*): Unit = {
-      atomic { implicit txn =>
-        revisionRef.transform { revision =>
-          commands.foldLeft(revision) { case (rev, command) =>
-            supervisor ! command.commandRequest.withExpectedRevision(rev)
-            expectMsgSuccess.response.tag.revision
-          }
-        }
+      commands.foreach { command =>
+        supervisor ! command
+        expectMsgSuccess
       }
     }
 
     /**
-     * Executes the specified command and returns the status from the aggregate.
-     *
-     * @param cmd to execute.
-     * @return status.
-     */
+      * Executes the specified command and returns the status from the aggregate.
+      *
+      * @param cmd to execute.
+      * @return status.
+      */
     def command(cmd: AggregateCommandMessage): AggregateStatus = {
       atomic { implicit txn =>
         val statusOptionRef: Ref[Option[AggregateStatus]] = Ref(None)
 
-        revisionRef.transform { revision =>
-          supervisor ! defaultHeadersOption.foldLeft(cmd.commandRequest.withExpectedRevision(revision))(_ withHeaders _)
-          expectMsgPF() {
-            case success: AggregateStatus.Success =>
-              statusOptionRef.set(Some(success))
-              success.response.tag.revision
+        supervisor ! defaultHeadersOption.foldLeft(cmd.commandRequest)(_ withHeaders _)
 
-            case failure@AggregateStatus.Failure(_) =>
-              statusOptionRef.set(Some(failure))
-              revision
-          }
+        expectMsgPF() {
+          case success: AggregateStatus.Success =>
+            statusOptionRef.set(Some(success))
+
+          case failure@AggregateStatus.Failure(_) =>
+            statusOptionRef.set(Some(failure))
         }
 
         statusOptionRef().getOrElse(throw new RuntimeException("Unexpected status"))
