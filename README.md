3fa2070f1a43c2798d769484486857489c8d3d07
Andre Kampert <andre@product-foundry.com>
2017-11-08 14:16:36 +0100

    Boost scala version to 2.11.11

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ To include this library into your `sbt` project, add the following lines to your
 
     libraryDependencies += "com.productfoundry" %% "akka-cqrs" % "0.10"
 
-This version of `akka-cqrs` is built using Akka 2.4.17 and Scala 2.11.8.
+This version of `akka-cqrs` is built using Akka 2.4.17 and Scala 2.11.11.
 
 Usage
 -----
--- project/AkkaCqrsBuild.scala
+++ project/AkkaCqrsBuild.scala
@@ -78,7 +78,7 @@ object AkkaCqrsBuild extends Build {
       javacOptions in compile ++= Seq("-encoding", "UTF-8", "-source", "1.8", "-target", "1.8", "-Xlint:unchecked"),
       javacOptions in doc ++= Seq("-encoding", "UTF-8", "-source", "1.8"),
 
-      scalaVersion := "2.11.8",
+      scalaVersion := "2.11.11",
 
       parallelExecution in Test := false,
 
